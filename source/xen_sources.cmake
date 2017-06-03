05d79b7fd431ed697e876612274cbfafc2fa2ab9
Jamie Terry <jnterry@ntlworld.com>
2017-06-03 14:00:48 +0100

    Updated source file list to include headers, so that generated cmake ide projects list header files correctly

--- source/xen_sources.cmake
+++ source/xen_sources.cmake
@@ -1,18 +1,61 @@
+##################################################
+## Core
+##################################################
 set(XEN_SOURCES_CORE
   ${CMAKE_CURRENT_LIST_DIR}/xen/core/memory/Allocator.cpp
   ${CMAKE_CURRENT_LIST_DIR}/xen/core/memory/ArenaLinear.cpp
-  )
+)
+
+set(XEN_HEADERS_CORE
+  ${CMAKE_CURRENT_LIST_DIR}/xen/core/intrinsics.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/core/memory.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/core/memory/Allocator.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/core/memory/ArenaLinear.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/core/memory/utilities.hpp
+)
+
+
+##################################################
+## Math
+##################################################
 
 set(XEN_SOURCES_MATH
   ${CMAKE_CURRENT_LIST_DIR}/xen/math/Quaternion.cpp
+  )
+
+set(XEN_HEADERS_MATH
+  ${CMAKE_CURRENT_LIST_DIR}/xen/math/Angle.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/math/Vector.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/math/Matrix.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/math/Quaternion.hpp
 )
+
+
+##################################################
+## Graphics
+##################################################
 
 set(XEN_SOURCES_GRAPHICS
   ${CMAKE_CURRENT_LIST_DIR}/xen/graphics/Shader.gl.cpp
+  )
+
+set(XEN_HEADERS_GRAPHICS
+  ${CMAKE_CURRENT_LIST_DIR}/xen/graphics/Shader.hpp
+  ${CMAKE_CURRENT_LIST_DIR}/xen/graphics/gl_header.hxx
 )
+
+##################################################
+## All
+##################################################
 
 set(XEN_SOURCES_ALL
   ${XEN_SOURCES_CORE}
   ${XEN_SOURCES_MATH}
   ${XEN_SOURCES_GRAPHICS}
+)
+
+set(XEN_HEADERS_ALL
+  ${XEN_HEADERS_CORE}
+  ${XEN_HEADERS_MATH}
+  ${XEN_HEADERS_GRAPHICS}
 )
