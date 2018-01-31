06ba2c68c14cc45b14aa07bf76271c13d66f77a4
Jamie Terry <jnterry@ntlworld.com>
2018-01-31 13:34:23 +0000

    Fix formatting

--- source/xen/math/Matrix.hpp
+++ source/xen/math/Matrix.hpp
@@ -299,17 +299,17 @@ xen::Matrix<T_Rows, T_Cols, T> operator-(xen::Matrix<T_Rows, T_Cols, T>& lhs, xe
 template<typename T>
 xen::Matrix<3,3, T> operator*(const xen::Matrix<3,3, T>& lhs, const xen::Matrix<3,3, T>& rhs){
 	return { lhs.elements[0]*rhs.elements[0] + lhs.elements[1]*rhs.elements[3] + lhs.elements[2]*rhs.elements[6]
-		   , lhs.elements[0]*rhs.elements[1] + lhs.elements[1]*rhs.elements[4] + lhs.elements[2]*rhs.elements[7]
-		   , lhs.elements[0]*rhs.elements[2] + lhs.elements[1]*rhs.elements[5] + lhs.elements[2]*rhs.elements[8]
+			    , lhs.elements[0]*rhs.elements[1] + lhs.elements[1]*rhs.elements[4] + lhs.elements[2]*rhs.elements[7]
+			    , lhs.elements[0]*rhs.elements[2] + lhs.elements[1]*rhs.elements[5] + lhs.elements[2]*rhs.elements[8]
 
-		   , lhs.elements[3]*rhs.elements[0] + lhs.elements[4]*rhs.elements[3] + lhs.elements[5]*rhs.elements[6]
-		   , lhs.elements[3]*rhs.elements[1] + lhs.elements[4]*rhs.elements[4] + lhs.elements[5]*rhs.elements[7]
-		   , lhs.elements[3]*rhs.elements[2] + lhs.elements[4]*rhs.elements[5] + lhs.elements[5]*rhs.elements[8]
+			    , lhs.elements[3]*rhs.elements[0] + lhs.elements[4]*rhs.elements[3] + lhs.elements[5]*rhs.elements[6]
+			    , lhs.elements[3]*rhs.elements[1] + lhs.elements[4]*rhs.elements[4] + lhs.elements[5]*rhs.elements[7]
+			    , lhs.elements[3]*rhs.elements[2] + lhs.elements[4]*rhs.elements[5] + lhs.elements[5]*rhs.elements[8]
 
-		   , lhs.elements[6]*rhs.elements[0] + lhs.elements[7]*rhs.elements[3] + lhs.elements[8]*rhs.elements[6]
-		   , lhs.elements[6]*rhs.elements[1] + lhs.elements[7]*rhs.elements[4] + lhs.elements[8]*rhs.elements[7]
-		   , lhs.elements[6]*rhs.elements[2] + lhs.elements[7]*rhs.elements[5] + lhs.elements[8]*rhs.elements[8]
-		   };
+			    , lhs.elements[6]*rhs.elements[0] + lhs.elements[7]*rhs.elements[3] + lhs.elements[8]*rhs.elements[6]
+			    , lhs.elements[6]*rhs.elements[1] + lhs.elements[7]*rhs.elements[4] + lhs.elements[8]*rhs.elements[7]
+			    , lhs.elements[6]*rhs.elements[2] + lhs.elements[7]*rhs.elements[5] + lhs.elements[8]*rhs.elements[8]
+			   };
 }
 
 template<typename T>
@@ -317,55 +317,55 @@ xen::Matrix<4,4, T> operator*(const xen::Matrix<4,4,T>& lhs, const xen::Matrix<4
 	return { lhs.elements[ 0] * rhs.elements[ 0] + lhs.elements[ 1] * rhs.elements[ 4] +
 	         lhs.elements[ 2] * rhs.elements[ 8] + lhs.elements[ 3] * rhs.elements[12]
 
-		   , lhs.elements[ 0] * rhs.elements[ 1] + lhs.elements[ 1] * rhs.elements[ 5] +
-	         lhs.elements[ 2] * rhs.elements[ 9] + lhs.elements[ 3] * rhs.elements[13]
+			   , lhs.elements[ 0] * rhs.elements[ 1] + lhs.elements[ 1] * rhs.elements[ 5] +
+			     lhs.elements[ 2] * rhs.elements[ 9] + lhs.elements[ 3] * rhs.elements[13]
 
-           , lhs.elements[ 0] * rhs.elements[ 2] + lhs.elements[ 1] * rhs.elements[ 6] +
-	         lhs.elements[ 2] * rhs.elements[10] + lhs.elements[ 3] * rhs.elements[14]
+			   , lhs.elements[ 0] * rhs.elements[ 2] + lhs.elements[ 1] * rhs.elements[ 6] +
+			     lhs.elements[ 2] * rhs.elements[10] + lhs.elements[ 3] * rhs.elements[14]
 
-		   , lhs.elements[ 0] * rhs.elements[ 3] + lhs.elements[ 1] * rhs.elements[ 7] +
-	         lhs.elements[ 2] * rhs.elements[11] + lhs.elements[ 3] * rhs.elements[15]
-
-
-		   , lhs.elements[ 4] * rhs.elements[ 0] + lhs.elements[ 5] * rhs.elements[ 4] +
-		     lhs.elements[ 6] * rhs.elements[ 8] + lhs.elements[ 7] * rhs.elements[12]
-
-		   , lhs.elements[ 4] * rhs.elements[ 1] + lhs.elements[ 5] * rhs.elements[ 5] +
-	         lhs.elements[ 6] * rhs.elements[ 9] + lhs.elements[ 7] * rhs.elements[13]
-
-		   , lhs.elements[ 4] * rhs.elements[ 2] + lhs.elements[ 5] * rhs.elements[ 6] +
-	         lhs.elements[ 6] * rhs.elements[10] + lhs.elements[ 7] * rhs.elements[14]
-
-		   , lhs.elements[ 4] * rhs.elements[ 3] + lhs.elements[ 5] * rhs.elements[ 7] +
-	         lhs.elements[ 6] * rhs.elements[11] + lhs.elements[ 7] * rhs.elements[15]
+			   , lhs.elements[ 0] * rhs.elements[ 3] + lhs.elements[ 1] * rhs.elements[ 7] +
+			     lhs.elements[ 2] * rhs.elements[11] + lhs.elements[ 3] * rhs.elements[15]
 
 
-		   , lhs.elements[ 8] * rhs.elements[ 0] + lhs.elements[ 9] * rhs.elements[ 4] +
-	         lhs.elements[10] * rhs.elements[ 8] + lhs.elements[11] * rhs.elements[12]
+			   , lhs.elements[ 4] * rhs.elements[ 0] + lhs.elements[ 5] * rhs.elements[ 4] +
+			     lhs.elements[ 6] * rhs.elements[ 8] + lhs.elements[ 7] * rhs.elements[12]
 
-		   , lhs.elements[ 8] * rhs.elements[ 1] + lhs.elements[ 9] * rhs.elements[ 5] +
-	         lhs.elements[10] * rhs.elements[ 9] + lhs.elements[11] * rhs.elements[13]
+			   , lhs.elements[ 4] * rhs.elements[ 1] + lhs.elements[ 5] * rhs.elements[ 5] +
+			     lhs.elements[ 6] * rhs.elements[ 9] + lhs.elements[ 7] * rhs.elements[13]
 
-		   , lhs.elements[ 8] * rhs.elements[ 2] + lhs.elements[ 9] * rhs.elements[ 6] +
-	         lhs.elements[10] * rhs.elements[10] + lhs.elements[11] * rhs.elements[14]
+			   , lhs.elements[ 4] * rhs.elements[ 2] + lhs.elements[ 5] * rhs.elements[ 6] +
+			     lhs.elements[ 6] * rhs.elements[10] + lhs.elements[ 7] * rhs.elements[14]
 
-		   , lhs.elements[ 8] * rhs.elements[ 3] + lhs.elements[ 9] * rhs.elements[ 7] +
-	         lhs.elements[10] * rhs.elements[11] + lhs.elements[11] * rhs.elements[15]
+			   , lhs.elements[ 4] * rhs.elements[ 3] + lhs.elements[ 5] * rhs.elements[ 7] +
+			     lhs.elements[ 6] * rhs.elements[11] + lhs.elements[ 7] * rhs.elements[15]
+
+
+			   , lhs.elements[ 8] * rhs.elements[ 0] + lhs.elements[ 9] * rhs.elements[ 4] +
+			     lhs.elements[10] * rhs.elements[ 8] + lhs.elements[11] * rhs.elements[12]
+
+			   , lhs.elements[ 8] * rhs.elements[ 1] + lhs.elements[ 9] * rhs.elements[ 5] +
+			     lhs.elements[10] * rhs.elements[ 9] + lhs.elements[11] * rhs.elements[13]
+
+			   , lhs.elements[ 8] * rhs.elements[ 2] + lhs.elements[ 9] * rhs.elements[ 6] +
+			     lhs.elements[10] * rhs.elements[10] + lhs.elements[11] * rhs.elements[14]
+
+			   , lhs.elements[ 8] * rhs.elements[ 3] + lhs.elements[ 9] * rhs.elements[ 7] +
+			     lhs.elements[10] * rhs.elements[11] + lhs.elements[11] * rhs.elements[15]
 
 
 
-		   , lhs.elements[12] * rhs.elements[ 0] + lhs.elements[13] * rhs.elements[ 4] +
-	         lhs.elements[14] * rhs.elements[ 8] + lhs.elements[15] * rhs.elements[12]
+			   , lhs.elements[12] * rhs.elements[ 0] + lhs.elements[13] * rhs.elements[ 4] +
+			     lhs.elements[14] * rhs.elements[ 8] + lhs.elements[15] * rhs.elements[12]
 
-		   , lhs.elements[12] * rhs.elements[ 1] + lhs.elements[13] * rhs.elements[ 5] +
-	         lhs.elements[14] * rhs.elements[ 9] + lhs.elements[15] * rhs.elements[13]
+			   , lhs.elements[12] * rhs.elements[ 1] + lhs.elements[13] * rhs.elements[ 5] +
+			     lhs.elements[14] * rhs.elements[ 9] + lhs.elements[15] * rhs.elements[13]
 
-		   , lhs.elements[12] * rhs.elements[ 2] +lhs.elements[13] * rhs.elements[ 6] +
-	         lhs.elements[14] * rhs.elements[10] +lhs.elements[15] * rhs.elements[14]
+			   , lhs.elements[12] * rhs.elements[ 2] +lhs.elements[13] * rhs.elements[ 6] +
+			     lhs.elements[14] * rhs.elements[10] +lhs.elements[15] * rhs.elements[14]
 
-		   , lhs.elements[12] * rhs.elements[ 3] +lhs.elements[13] * rhs.elements[ 7] +
-	         lhs.elements[14] * rhs.elements[11] +lhs.elements[15] * rhs.elements[15]
-	};
+			   , lhs.elements[12] * rhs.elements[ 3] +lhs.elements[13] * rhs.elements[ 7] +
+			     lhs.elements[14] * rhs.elements[11] +lhs.elements[15] * rhs.elements[15]
+			};
 }
 
 template<typename T>
