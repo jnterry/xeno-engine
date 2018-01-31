47ce8524819678d19e8d5ecdd446ce2cb1cc4e66
Jamie Terry <jnterry@ntlworld.com>
2018-01-31 13:35:11 +0000

    Add Mat3 inverse code

--- source/xen/math/Matrix.hpp
+++ source/xen/math/Matrix.hpp
@@ -37,9 +37,12 @@ namespace xen{
 		//	Vec<3, T> rows[3]; //:TODO: not sure if in row major or column major format yet...
 		//};
 		static const Matrix<3,3,T> Identity;
+		static const Matrix<3,3,T> Zero;
 	};
 	template<typename T>
 	Matrix<3,3,T> const Matrix<3,3,T>::Identity = {1,0,0,   0,1,0,  0,0,1};
+	template<typename T>
+	Matrix<3,3,T> const Matrix<3,3,T>::Zero     = {0,0,0,   0,0,0,  0,0,0};
 
 	template<typename T>
 	struct Matrix<4,4,T>{
@@ -48,9 +51,12 @@ namespace xen{
 		//	Vec<4, T> rows[4]; //:TODO: not sure if in row major or column major format yet...
 		//};
 		static const Matrix<4,4,T> Identity;
+		static const Matrix<4,4,T> Zero;
 	};
 	template<typename T>
 	Matrix<4,4,T> const Matrix<4,4,T>::Identity = {1,0,0,0,   0,1,0,0,  0,0,1,0,  0,0,0,1};
+	template<typename T>
+	Matrix<4,4,T> const Matrix<4,4,T>::Zero     = {0,0,0,0,   0,0,0,0,  0,0,0,0,  0,0,0,0};
 }
 
 #pragma GCC diagnostic pop // re-enable -Wpedantic
@@ -147,7 +153,101 @@ namespace xen{
 	     e[2] * e[5] * e[ 8] * e[15] + e[1] * e[6] * e[ 8] * e[15] +
 	     e[2] * e[4] * e[ 9] * e[15] - e[0] * e[6] * e[ 9] * e[15] -
 	     e[1] * e[4] * e[10] * e[15] + e[0] * e[5] * e[10] * e[15];
+	}
 
+	// Returns the inverse of the matrix given
+	// BEWARE: Here lies poorly documented spaghetti code
+	// It shouldn't need altering unless I've missed some null/swap cases
+	template <typename T>
+	Mat3<T> getInverse(const Mat3<T>& mat) {
+		Mat3<T> become_ident = mat; // Matrix that becomes identity as other becomes inverse
+		Mat3<T> become_invrs  = Mat3<T>::Identity; // Matrix that becomes the inverse via Gauss-Jordann
+
+	  T ratio;
+		// Zero first element of second row
+		ratio = mat.elements[1] / mat.elements[0];
+		become_ident.elements[1] -= ratio * become_ident.elements[0];
+		become_ident.elements[4] -= ratio * become_ident.elements[3];
+		become_ident.elements[7] -= ratio * become_ident.elements[6];
+		become_invrs.elements[1] -= ratio * become_invrs.elements[0];
+		become_invrs.elements[4] -= ratio * become_invrs.elements[3];
+		become_invrs.elements[7] -= ratio * become_invrs.elements[6];
+
+		// Zero first element of third row
+		ratio = mat.elements[2] / mat.elements[0];
+		become_ident.elements[2] -= ratio * become_ident.elements[0];
+		become_ident.elements[5] -= ratio * become_ident.elements[3];
+		become_ident.elements[8] -= ratio * become_ident.elements[6];
+		become_invrs.elements[2] -= ratio * become_invrs.elements[0];
+		become_invrs.elements[5] -= ratio * become_invrs.elements[3];
+		become_invrs.elements[8] -= ratio * become_invrs.elements[6];
+
+		// Check rows do not zero and do not require swap
+		if ((become_ident.elements[4] == 0) && (become_ident.elements[7] == 0)) {
+			return Mat3<T>::Zero;
+		} else if ((become_ident.elements[4] == 0)) {
+			if (become_ident.elements[5] == 0) {
+				return Mat3<T>::Zero;
+			} else {
+				become_ident.swapRow(1,2);
+			}
+		}
+
+		// Zero second element of third row
+		ratio = become_ident.elements[5] / become_ident.elements[4];
+		become_ident.elements[5] -= ratio * become_ident.elements[4];
+		become_ident.elements[8] -= ratio * become_ident.elements[7];
+		become_invrs.elements[2] -= ratio * become_invrs.elements[1];
+		become_invrs.elements[5] -= ratio * become_invrs.elements[4];
+		become_invrs.elements[8] -= ratio * become_invrs.elements[7];
+
+		// Check rows do not zero and do not require swap
+		if (become_ident.elements[8] == 0) {
+			return Mat3<T>::Zero;
+		}
+
+		// Zero out last column of first row
+		ratio = become_ident.elements[6] / become_ident.elements[8];
+		become_ident.elements[6] -= ratio * become_ident.elements[8];
+		become_invrs.elements[0] -= ratio * become_invrs.elements[2];
+		become_invrs.elements[3] -= ratio * become_invrs.elements[5];
+		become_invrs.elements[6] -= ratio * become_invrs.elements[8];
+
+		// Zero out last column of second row
+		ratio = become_ident.elements[7] / become_ident.elements[8];
+		become_ident.elements[7] -= ratio * become_ident.elements[8];
+		become_invrs.elements[1] -= ratio * become_invrs.elements[2];
+		become_invrs.elements[4] -= ratio * become_invrs.elements[5];
+		become_invrs.elements[7] -= ratio * become_invrs.elements[8];
+
+		// Zero out middle column of first row
+		ratio = become_ident.elements[3] / become_ident.elements[4];
+		become_ident.elements[3] -= ratio * become_ident.elements[4];
+		become_invrs.elements[0] -= ratio * become_invrs.elements[1];
+		become_invrs.elements[3] -= ratio * become_invrs.elements[4];
+		become_invrs.elements[6] -= ratio * become_invrs.elements[7];
+
+		// Make becomeIdentity the identity
+		// First row
+		ratio = become_ident.elements[0];
+		become_ident.elements[0] /= ratio;
+		become_invrs.elements[0] /= ratio;
+		become_invrs.elements[3] /= ratio;
+		become_invrs.elements[6] /= ratio;
+		// Second row
+		ratio = become_ident.elements[4];
+		become_ident.elements[4] /= ratio;
+		become_invrs.elements[1] /= ratio;
+		become_invrs.elements[4] /= ratio;
+		become_invrs.elements[7] /= ratio;
+		// Third row
+		ratio = become_ident.elements[8];
+		become_ident.elements[8] /= ratio;
+		become_invrs.elements[2] /= ratio;
+		become_invrs.elements[5] /= ratio;
+		become_invrs.elements[8] /= ratio;
+
+		return become_invrs;
 	}
 }
 
