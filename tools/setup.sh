4aa4377524bd1e2c0304154157334ababf9bd1bc
Jamie Terry <jnterry@ntlworld.com>
2018-02-15 19:36:06 +0000

    Move setup script into tools

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 # SFML dependencies
 sudo apt install libxrandr-dev libudev-dev libgl-dev libjpeg-dev libopenal-dev libflac-dev
 
-rm -rf build/
-mkdir build
-cd build
+rm -rf ../build/
+mkdir ../build/
+cd ../build/
 cmake ..
