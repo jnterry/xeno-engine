78dd57f1b35771502e609d24d64cbe78e9cfe83e
Jamie Terry <jnterry@ntlworld.com>
2018-03-05 11:40:05 +0000

    Fix flip on y axis when presenting to screen

--- source/xen/sren/RenderTargetImpl.unix.cpp
+++ source/xen/sren/RenderTargetImpl.unix.cpp
@@ -197,7 +197,7 @@ namespace xen {
 					              xen::mapToRange<float, u32>(0.0f, 1.0f, 0, 255, color.g) <<  8 |
 					              xen::mapToRange<float, u32>(0.0f, 1.0f, 0, 255, color.b) <<  0);
 
-					pixels[y * target.rows + x] = color_bits;
+					pixels[(target.cols - y - 1) * target.rows + x] = color_bits;
 				}
 			}
 
