53541bdbd330192a6ddc42e99adc5076b229ff87
Jamie Terry <jnterry@ntlworld.com>
2018-03-16 15:42:17 +0000

    Fix triangle clipping in z planes

--- source/xen/sren/renderer3d_rasterizer.cpp
+++ source/xen/sren/renderer3d_rasterizer.cpp
@@ -24,6 +24,8 @@
 #include <cstdlib>
 #include <float.h>
 
+#define XEN_SREN_DEBUG_RENDER_WIREFRAME
+
 namespace {
 
 	const xen::Aabb3r CLIP_SPACE_AABB = {
@@ -53,10 +55,15 @@ namespace {
 		}
 
 		for(u32 i = 0; i < geom.vertex_count; ++i){
-			Vec3f clip_space = geom.position[i] * mvp_matrix;
+			Vec4f clip_space = xen::toHomo(geom.position[i]) * mvp_matrix;
 
-			if(!xen::contains(CLIP_SPACE_AABB, clip_space)){
-					// Then point is not in view of the camera
+			if(clip_space.x < -clip_space.w ||
+			   clip_space.x >  clip_space.w ||
+			   clip_space.y < -clip_space.w ||
+			   clip_space.y >  clip_space.w ||
+			   clip_space.z < -clip_space.w ||
+			   clip_space.z >  clip_space.w){
+				// Then point is not in view of the camera
 				continue;
 			}
 
@@ -65,7 +72,7 @@ namespace {
 
 			// Convert from [-1, 1] clip space to screen space
 			Vec2r screen_space =
-				_convertToScreenSpace<Vec3r, Vec2r>(clip_space, viewport);
+				_convertToScreenSpace<Vec4r, Vec2r>(clip_space, viewport);
 
 			u32 pixel_index = (u32)screen_space.y*target.width + (u32)screen_space.x;
 
@@ -144,22 +151,44 @@ namespace {
 	                    xen::Color4f color,
 	                    const xen::LineSegment3r& line){
 
-		xen::LineSegment3r line_clip  = line * mvp_matrix;
+				xen::LineSegment4r line_clip  = xen::toHomo(line) * mvp_matrix;
 
-		// Clip to clip space, bail out if no intersection
-		if(!xen::intersect(line_clip, xen::Aabb3r{Vec3r{-1,-1,-1}, Vec3r{1,1,1}})){
+		///////////////////////////////////////////////////////////////////
+		// Do line clipping
+		if(line_clip.p1.z < 0 && line_clip.p2.z < 0){
+			// Then line is completely behind the camera
 			return;
 		}
+		if(line_clip.p1.z < 0){
+			// Then just one point is behind the camera, slide it along the direction
+			// of the line until its at same z as camera
+			Vec4r dir = line_clip.p2 - line_clip.p1;
+			line_clip.p1 += dir * ((-line_clip.p1.z) / dir.z);
+		}
+		if(line_clip.p2.z < 0){
+			Vec4r dir = line_clip.p1 - line_clip.p2;
+			line_clip.p2 += dir * ((-line_clip.p2.z) / dir.z);
+		}
+		real z_start = line_clip.p2.z;
+		real z_end   = line_clip.p1.z;
+		///////////////////////////////////////////////////////////////////
 
 		///////////////////////////////////////////////////////////////////
 		// Do perspective divide (into normalized device coordinates -> [-1, 1]
-		line_clip.p1.xy /= (line_clip.p1.z);
-		line_clip.p2.xy /= (line_clip.p2.z);
+		// We only care about x and y coordinates at this point
+		line_clip.p1.xy /= (line_clip.p1.w);
+		line_clip.p2.xy /= (line_clip.p2.w);
 		///////////////////////////////////////////////////////////////////
 
+		///////////////////////////////////////////////////////////////////
+		// Clip to the viewport
 		xen::LineSegment2r line_screen =
-			_convertToScreenSpace<xen::LineSegment3r, xen::LineSegment2r>(line_clip, viewport);
-		doRenderLine2d(target, line_screen, color, line_clip.p2.z, line_clip.p1.z);
+			_convertToScreenSpace<xen::LineSegment4r, xen::LineSegment2r>(line_clip, viewport);
+		if(xen::intersect(line_screen, viewport)){
+			doRenderLine2d(target, line_screen, color, z_start, z_end);
+		}
+		///////////////////////////////////////////////////////////////////
+
 	}
 
 	// Bresenham Triangle Algorithm, inspired by
@@ -320,8 +349,6 @@ namespace {
 		points_behind |= (tri_clip.p2.z < 0) << 1;
 		points_behind |= (tri_clip.p3.z < 0) << 2;
 
-		printf("Drawing triangle case %i\n", points_behind);
-
 		///////////////////////////////////////////////////////////////////
 		// Render the triangle(s)
 		// Various cases depending on which of the points are behind the camera
@@ -337,7 +364,24 @@ namespace {
 			xen::Triangle2r tri_screen =
 				_convertToScreenSpace<xen::Triangle4r, xen::Triangle2r>(tri_clip, viewport);
 
+			#ifndef XEN_SREN_DEBUG_RENDER_WIREFRAME
 			doRenderTriangle2d(target, viewport, color, tri_screen);
+			#else
+			xen::LineSegment2r line;
+
+			line.p1 = tri_screen.vertices[0];
+			line.p2 = tri_screen.vertices[1];
+			if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+			line.p1 = tri_screen.vertices[1];
+			line.p2 = tri_screen.vertices[2];
+			if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+			line.p1 = tri_screen.vertices[2];
+			line.p2 = tri_screen.vertices[0];
+			if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+			#endif
+
 			return;
 		}
 
@@ -349,18 +393,18 @@ namespace {
 			goto do_draw_2_behind;
 		case 6:   // 110
 		do_draw_2_behind: {
-				printf("Doing draw 2 behind case\n");
 				// There are two points behind the camera, so just make a new triangle
 				// between the single point in front of the camera, and where the lines
 				// intersect z = 0 plane.
 				//
 				// Move point p2 and p3 along the line connecting them to p1 such that z
 				// component becomes 0
+
 				Vec4r delta_p2 = (tri_clip.p1 - tri_clip.p2);
-				tri_clip.p2 += (delta_p2 / delta_p2.z) * -tri_clip.p2.z;
+				tri_clip.p2 += delta_p2 * ((-tri_clip.p2.z / delta_p2.z));
 
 				Vec4r delta_p3 = (tri_clip.p1 - tri_clip.p3);
-				tri_clip.p3 += (delta_p3 / delta_p3.z) * -tri_clip.p3.z;
+				tri_clip.p3 += delta_p3 * ((-tri_clip.p3.z / delta_p3.z));
 
 				// Do perspective divide (into normalized device coordinates -> [-1, 1]
 				tri_clip.p1 /= (tri_clip.p1.w);
@@ -370,7 +414,24 @@ namespace {
 				xen::Triangle2r tri_screen =
 					_convertToScreenSpace<xen::Triangle4r, xen::Triangle2r>(tri_clip, viewport);
 
+				#ifndef XEN_SREN_DEBUG_RENDER_WIREFRAME
 				doRenderTriangle2d(target, viewport, color, tri_screen);
+				#else
+				xen::LineSegment2r line;
+
+				line.p1 = tri_screen.vertices[0];
+				line.p2 = tri_screen.vertices[1];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+				line.p1 = tri_screen.vertices[1];
+				line.p2 = tri_screen.vertices[2];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+				line.p1 = tri_screen.vertices[2];
+				line.p2 = tri_screen.vertices[0];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+				#endif
+
 				return;
 			}
 		case 1: // 001
@@ -387,11 +448,14 @@ namespace {
 				//
 				// p3 is the point behind the camera, slide it to z = 0 along the lines
 				// joining it to p1 and p2
+				//Vec4r delta_p3 = (tri_clip.p1 - tri_clip.p3);
+				//tri_clip.p3 += delta_p3 * ((-tri_clip.p3.z / delta_p3.z));
+
 				Vec4r delta_p1       = (tri_clip.p1 - tri_clip.p3);
-				Vec4r p3_slide_to_p1 = (tri_clip.p3 + ((delta_p1 / delta_p1.z) * -tri_clip.p3.z));
+				Vec4r p3_slide_to_p1 = (tri_clip.p3 + (delta_p1 * ((-tri_clip.p3.z / delta_p1.z))));
 
 				Vec4r delta_p2       = (tri_clip.p2 - tri_clip.p3);
-				Vec4r p3_slide_to_p2 = (tri_clip.p3 + ((delta_p2 / delta_p2.z) * -tri_clip.p3.z));
+				Vec4r p3_slide_to_p2 = (tri_clip.p3 + (delta_p2 * ((-tri_clip.p3.z / delta_p2.z))));
 
 				xen::VertexGroup2r<4> quad_screen;
 				quad_screen.vertices[0] = (p3_slide_to_p1 / p3_slide_to_p1.w).xy;
@@ -402,16 +466,38 @@ namespace {
 				quad_screen =
 					_convertToScreenSpace<xen::VertexGroup2r<4>, xen::VertexGroup2r<4>>(quad_screen, viewport);
 
+				#ifndef XEN_SREN_DEBUG_RENDER_WIREFRAME
 				doRenderTriangle2d(target, viewport, color, *(xen::Triangle2r*)&quad_screen.vertices[0]);
 				doRenderTriangle2d(target, viewport, color, *(xen::Triangle2r*)&quad_screen.vertices[1]);
+				#else
+				xen::LineSegment2r line;
+
+				line.p1 = quad_screen.vertices[0];
+				line.p2 = quad_screen.vertices[1];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+				line.p1 = quad_screen.vertices[1];
+				line.p2 = quad_screen.vertices[2];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+				line.p1 = quad_screen.vertices[2];
+				line.p2 = quad_screen.vertices[0];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+				line.p1 = quad_screen.vertices[2];
+				line.p2 = quad_screen.vertices[3];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+
+				line.p1 = quad_screen.vertices[3];
+				line.p2 = quad_screen.vertices[0];
+				if(xen::intersect(line, viewport)){ doRenderLine2d(target, line, color, 0, 0); }
+				#endif
 			}
 			return;
 		}
 		///////////////////////////////////////////////////////////////////
 	}
 }
-
-
 
 namespace xen{
 
