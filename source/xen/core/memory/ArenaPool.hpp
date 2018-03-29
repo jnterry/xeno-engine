885edd9a8aa448a3745fc93990134bdd642e8ff3
Jamie Terry <jnterry@ntlworld.com>
2018-03-29 11:21:23 +0100

    Implement ArenaPool, add basic unit tests

--- source/xen/core/memory/ArenaPool.hpp
+++ source/xen/core/memory/ArenaPool.hpp
@@ -10,16 +10,19 @@
 #define XEN_CORE_ARENAPOOL_HPP
 
 #include <xen/core/intrinsics.hpp>
+#include <xen/core/memory/Allocator.hpp>
+#include <xen/core/memory/utilities.hpp>
 
 namespace xen {
-
-	class Allocator;
-
 	/////////////////////////////////////////////////////////////////////
 	/// \brief Represents a fixed size pool of items of some type
 	/////////////////////////////////////////////////////////////////////
 	template<typename T>
-	struct ArenaPool {
+	struct ArenaPool : public xen::NonCopyable {
+		/// \brief Value used for Slot::next_free to indicate
+		/// there is no next free slot
+		static const constexpr u32 BAD_SLOT_INDEX = 0xFFFFFFFF;
+
 		/// \brief Represents a single slot in the pool
 		union Slot {
 			/// \brief The object of type T stored in this slot
@@ -44,41 +47,47 @@ namespace xen {
 		/// Note that we use array indices rather than pointers to save memory
 		/// if sizeof(T) < sizeof(Slot*)
 		u32 first_free_slot;
-	};
 
-	/////////////////////////////////////////////////////////////////////
-	/// \brief Represents a (potentially) resizable pool of of items of
-	/// some type
-	/////////////////////////////////////////////////////////////////////
-	template<typename T>
-	struct ArenaPool {
-		static_assert(sizeof(T) >= sizeof(u32));
+		ArenaPool(){
+			// no-op
+		}
 
-
-
-		/// \brief Represents a block of pool Slot instances
-		struct Block {
-			/// \brief Pointer to the array of slots in this Block
-			PoolSlot*  slots;
-
-			/// \brief Index of the first free slot in this block
-			u32        first_free;
-
-			/// \brief The number of slots
-			u32        capacity;
-			u32        slots_used;
-			PoolBlock* next_block;
-		};
-
-		Allocator* backing_allocator;
-		PoolBlock* first_block;
-		u32        capacity;
-		u32        slots_used;
-		PoolBlock* first_block;
+		ArenaPool (ArenaPool&& other) :
+			slots(other.slots), capacity(other.capacity),
+			slots_used(other.slots_used) {
+			// no-op
+		}
 	};
 
 	template<typename T>
-	ArenaPool createArenaPool(Allocator* allocator, u32 capacity);
+	ArenaPool<T> createArenaPool(Allocator* allocator, u32 capacity){
+		ArenaPool<T> result;
+		xen::clearToZero(&result);
+
+		////////////////////////////////////////////////////////////////////
+		// Allocate storage
+		result.slots = (typename ArenaPool<T>::Slot*)
+			allocator->allocate(sizeof(T) * capacity, alignof(T));
+
+		if(result.slots == nullptr){
+			return result;
+		}
+
+		////////////////////////////////////////////////////////////////////
+		// Initialise members
+		result.capacity        = capacity;
+		result.slots_used      = 0;
+		result.first_free_slot = 0;
+
+		////////////////////////////////////////////////////////////////////
+		// Initialise the slot linked list to have each point at the next
+		for(u32 i = 0; i < capacity-1; ++i){
+			result.slots[i].next_free = i + 1;
+		}
+		result.slots[capacity-1].next_free = ArenaPool<T>::BAD_SLOT_INDEX;
+
+		return result;
+	}
 
 	/////////////////////////////////////////////////////////////////////
 	/// \brief Deallocates all memory associated with the specified ArenaPool
@@ -86,7 +95,9 @@ namespace xen {
 	/// This invalidates any items allocated from the pool
 	/////////////////////////////////////////////////////////////////////
 	template<typename T>
-	ArenaPool destroyArenaPool(Allocator* allocator);
+  void destroyArenaPool(Allocator* allocator, ArenaPool<T>& pool){
+		allocator->deallocate(pool.slots);
+	}
 
 	/////////////////////////////////////////////////////////////////////
   /// \brief Reserves a slot from the specified ArenaPool for an instance of
@@ -94,26 +105,34 @@ namespace xen {
 	/// \public \memberof xen::ArenaPool
 	/////////////////////////////////////////////////////////////////////
 	template<typename T>
-	T* reserveType(ArenaPool<T> arena){
-
-	  return (T*)reserveBytes(arena, sizeof(T), alignof(T));
+	T* reserveType(ArenaPool<T>& arena){
+		if(arena.first_free_slot == ArenaPool<T>::BAD_SLOT_INDEX){
+			return nullptr;
+		}
+		++arena.slots_used;
+		typename ArenaPool<T>::Slot* slot = &arena.slots[arena.first_free_slot];
+		arena.first_free_slot = slot->next_free;
+		return &slot->item;
 	}
 
-	/// \brief Frees
+	/////////////////////////////////////////////////////////////////////
+	/// \brief Frees an instance of some type, allowing the slot in the specified
+	/// pool arena to be reused in a future allocation
+	/////////////////////////////////////////////////////////////////////
 	template<typename T>
-	T* freeType(ArenaPool<T>& arena) {
-
+	void freeType(ArenaPool<T>& arena, T* object) {
+		u32 slot_index = (object - &arena.slots[0].item) / sizeof(T);
+		--arena.slots_used;
+		arena.slots[slot_index].next_free = arena.first_free_slot;
+		arena.first_free_slot = slot_index;
 	}
 
 	/// \brief Pushes a new instance of some class into some arena. After
 	/// allocation calls the class's constructor with specified arguments
 	template<typename T, typename... T_ARGS>
-	inline T* emplace(ArenaLinear& arena, T_ARGS... args){
+	inline T* emplace(ArenaPool<T>& arena, T_ARGS... args){
 		return new (reserveType<T>(arena)) (T)(args...);
 	}
-
-
-
 }
 
 #endif
---
+++ tests/core/ArenaPool.cpp
@@ -0,0 +1,79 @@
+#include <xen/core/memory/ArenaPool.hpp>
+#include <xen/core/memory/Allocator.hpp>
+#include <catch.hpp>
+
+xen::Allocator* allocator = new xen::AllocatorMalloc();
+
+struct Thing {
+	int x, y;
+};
+
+TEST_CASE("ArenaPool", "[core][memory][ArenaPool]"){
+	xen::ArenaPool<Thing> pool = xen::createArenaPool<Thing>(allocator, 4);
+
+	REQUIRE(pool.slots != nullptr);
+	REQUIRE(pool.capacity   == 4);
+	REQUIRE(pool.slots_used == 0);
+
+	SECTION("Single allocation"){
+		Thing* t = xen::reserveType<Thing>(pool);
+
+		t->x = 10; // write to thing to make sure this doesn't corrupt the pool
+		t->y = 20;
+
+		CHECK(pool.capacity   ==  4);
+		CHECK(pool.slots_used ==  1);
+		CHECK(t->x            == 10);
+		CHECK(t->y            == 20);
+	}
+
+	SECTION("Too many allocations"){
+		Thing* ta = xen::reserveType<Thing>(pool);
+		Thing* tb = xen::reserveType<Thing>(pool);
+		Thing* tc = xen::reserveType<Thing>(pool);
+		Thing* td = xen::reserveType<Thing>(pool);
+		Thing* te = xen::reserveType<Thing>(pool);
+
+		REQUIRE(te == nullptr);
+
+		ta->x = 10; // write to thing to make sure this doesn't corrupt the pool
+		ta->y = 11;
+		tb->x = 12; // Also check that each instance is really separate and doesn't
+		tb->y = 13; // overwrite memory of another
+		tc->x = 14;
+		tc->y = 15;
+		td->x = 16;
+		td->y = 17;
+
+		CHECK(pool.capacity   ==  4);
+		CHECK(pool.slots_used ==  4);
+		CHECK(ta->x           == 10);
+		CHECK(ta->y           == 11);
+		CHECK(tb->x           == 12);
+		CHECK(tb->y           == 13);
+		CHECK(tc->x           == 14);
+		CHECK(tc->y           == 15);
+		CHECK(td->x           == 16);
+		CHECK(td->y           == 17);
+
+		xen::freeType<Thing>(pool, ta);
+
+		CHECK(pool.capacity   ==  4);
+		CHECK(pool.slots_used ==  3);
+
+		Thing* tf = xen::reserveType<Thing>(pool);
+		tf->x = 18;
+		tf->y = 19;
+
+		CHECK(pool.capacity   ==  4);
+		CHECK(pool.slots_used ==  4);
+		CHECK(tf->x           == 18);
+		CHECK(tf->y           == 19);
+		CHECK(tb->x           == 12);
+		CHECK(tb->y           == 13);
+		CHECK(tc->x           == 14);
+		CHECK(tc->y           == 15);
+		CHECK(td->x           == 16);
+		CHECK(td->y           == 17);
+	}
+}
