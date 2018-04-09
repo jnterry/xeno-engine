221854ea4b9e1ab9bbb4e6e8663a8eda3c812e4b
Saxon Zerbino <sz15266@my.bristol.ac.uk>
2018-04-09 20:48:58 +0100

    Add code to calculate average luma in region of pixel and determine if this is within edge threshold

--- source/xen/sren/post_processors/Antialias.cpp
+++ source/xen/sren/post_processors/Antialias.cpp
@@ -13,6 +13,7 @@
 #define XEN_SREN_POSTPROCESSORS_ANTIALIAS_CPP
 
 #include <xen/sren/PostProcessor.hpp>
+#include <stdlib.h>
 
 // Recommended values for constants to determine presence of edge
 #define EDGE_THRESHOLD_MIN 0.0312
@@ -21,7 +22,7 @@
 namespace xen {
 	namespace sren {
 
-		float rgb2luma(Vec3f rgb){
+		real rgb2luma(Vec3f rgb){
 		    return sqrt(dot(rgb, Vec3f(0.299, 0.587, 0.114)));
 		}
 
@@ -29,27 +30,89 @@ namespace xen {
 			int currentPixel = y*fb.size.x + x;
 
 			// Luma at the current fragment
-			float lumaCenter = rgb2luma(fb.color[currentPixel].rgb);
+			real lumaCenter = rgb2luma(fb.color[currentPixel].rgb);
 			// :TODO: What to do if these are out of bounds?
+			// :TODO: Check if top left or bottom left is fb[0], if bottom left then swap up and down
 			// Luma at the four direct neighbours of the current fragment.
-			float lumaDown   = rgb2luma(fb.color[(y+1)*fb.size.x + (x  )].rgb);
-			float lumaUp     = rgb2luma(fb.color[(y-1)*fb.size.x + (x  )].rgb);
-			float lumaLeft   = rgb2luma(fb.color[(y  )*fb.size.x + (x-1)].rgb);
-			float lumaRight  = rgb2luma(fb.color[(y  )*fb.size.x + (x+1)].rgb);
+			real lumaDown   = rgb2luma(fb.color[(y+1)*fb.size.x + (x  )].rgb);
+			real lumaUp     = rgb2luma(fb.color[(y-1)*fb.size.x + (x  )].rgb);
+			real lumaLeft   = rgb2luma(fb.color[(y  )*fb.size.x + (x-1)].rgb);
+			real lumaRight  = rgb2luma(fb.color[(y  )*fb.size.x + (x+1)].rgb);
 
 			// Find the maximum and minimum luma around the current fragment.
-			float lumaMin = min(lumaCenter, lumaDown, lumaUp, lumaLeft, lumaRight);
-			float lumaMax = max(lumaCenter, lumaDown, lumaUp, lumaLeft, lumaRight);
+			real lumaMin = min(lumaCenter, lumaDown, lumaUp, lumaLeft, lumaRight);
+			real lumaMax = max(lumaCenter, lumaDown, lumaUp, lumaLeft, lumaRight);
 			// Compute the delta.
-			float lumaRange = lumaMax - lumaMin;
+			real lumaRange = lumaMax - lumaMin;
 
 			// If the luma variation is lower that a threshold (or if we are in a really dark area),
 			// we are not on an edge, don't perform any AA.
 			if(lumaRange < max(EDGE_THRESHOLD_MIN,lumaMax*EDGE_THRESHOLD_MAX)){
     		return;
 			}
-			// :TODO: Complete FXAA tutorial, as above
-			//        Finsished as far as "Estimating gradient and choosing edge direction"
+
+			// Query the 4 remaining corners lumas.
+			real lumaDownLeft  = rgb2luma(fb.color[(y+1)*fb.size.x + (x-1)].rgb);
+			real lumaUpRight   = rgb2luma(fb.color[(y-1)*fb.size.x + (x+1)].rgb);
+			real lumaUpLeft    = rgb2luma(fb.color[(y-1)*fb.size.x + (x-1)].rgb);
+			real lumaDownRight = rgb2luma(fb.color[(y+1)*fb.size.x + (x+1)].rgb);
+
+			// Combine the four edges lumas (using intermediary variables for future computations with the same values).
+			real lumaDownUp = lumaDown + lumaUp;
+			real lumaLeftRight = lumaLeft + lumaRight;
+			// Same for corners
+			real lumaLeftCorners = lumaDownLeft + lumaUpLeft;
+			real lumaDownCorners = lumaDownLeft + lumaDownRight;
+			real lumaRightCorners = lumaDownRight + lumaUpRight;
+			real lumaUpCorners = lumaUpRight + lumaUpLeft;
+
+			// Compute an estimation of the gradient along the horizontal and vertical axis.
+			real edgeHorizontal = std::abs(-2.0 * lumaLeft + lumaLeftCorners)
+			                    + std::abs(-2.0 * lumaCenter + lumaDownUp ) * 2.0
+													+ std::abs(-2.0 * lumaRight + lumaRightCorners);
+			real edgeVertical   = std::abs(-2.0 * lumaUp + lumaUpCorners)
+			                    + std::abs(-2.0 * lumaCenter + lumaLeftRight) * 2.0
+													+ std::abs(-2.0 * lumaDown + lumaDownCorners);
+
+			// Is the local edge horizontal or vertical ?
+			bool isHorizontal = (edgeHorizontal >= edgeVertical);
+
+			// Select the two neighboring pixel lumas in the opposite direction to the local edge.
+			real luma1 = isHorizontal ? lumaDown : lumaLeft;
+			real luma2 = isHorizontal ? lumaUp : lumaRight;
+			// Compute gradients in this direction.
+			real gradient1 = luma1 - lumaCenter;
+			real gradient2 = luma2 - lumaCenter;
+
+			// Which direction is the steepest ?
+			bool is1Steepest = std::abs(gradient1) >= std::abs(gradient2);
+
+			// Gradient in the corresponding direction, normalized.
+			real gradientScaled = 0.25*max(std::abs(gradient1),std::abs(gradient2));
+			// Choose the step size (one pixel) according to the edge direction.
+			real stepLength = isHorizontal ? inverseScreenSize.y : inverseScreenSize.x;
+
+			// Average luma in the correct direction.
+			real lumaLocalAverage = 0.0;
+
+			if(is1Steepest){
+    		// Switch the direction
+    		stepLength = - stepLength;
+    		lumaLocalAverage = 0.5*(luma1 + lumaCenter);
+			} else {
+    		lumaLocalAverage = 0.5*(luma2 + lumaCenter);
+			}
+
+			// :TODO: How to translate this into xen code?
+			// Shift UV in the correct direction by half a pixel.
+			Vec2r currentUv = In.uv;
+			if(isHorizontal){
+    		currentUv.y += stepLength * 0.5;
+			} else {
+    		currentUv.x += stepLength * 0.5;
+			}
+
+			// :TODO: Continue from 'First iteration exploration' in tutorial
 		}
 
 		void PostProcessorAntialias::process(Framebuffer& fb) {
