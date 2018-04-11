f8379d08dd5cf2ed7e49face40491beb33777ab5
Saxon Zerbino <sz15266@my.bristol.ac.uk>
2018-04-11 14:23:12 +0100

    WIP: Add code to find edges of current line into fxaaStep, currently need to implement bilinear filtering to continue

--- source/xen/sren/post_processors/Antialias.cpp
+++ source/xen/sren/post_processors/Antialias.cpp
@@ -9,6 +9,13 @@
 // GPU based example of FXAA: http://blog.simonrodriguez.fr/articles/30-07-2016_implementing_fxaa.html
 // FXAA Whitepaper: http://developer.download.nvidia.com/assets/gamedev/files/sdk/11/FXAA_WhitePaper.pdf
 
+// :TODO: when making pixel access, if float does not land exactly on pixel:
+//        lerp between four nearest pixels, weighted by distance to each -> bilinear filtering
+//        This gives 'free' interpolation
+// :TODO: What to do if Any luma are out of bounds, such as pixels on edges?
+// :TODO: Check if top left or bottom left is fb[0], if bottom left then swap lumaUp and lumaDown
+// :TODO: Define QUALITY(i) -> currently don't know how
+
 #ifndef XEN_SREN_POSTPROCESSORS_ANTIALIAS_CPP
 #define XEN_SREN_POSTPROCESSORS_ANTIALIAS_CPP
 
@@ -18,6 +25,8 @@
 // Recommended values for constants to determine presence of edge
 #define EDGE_THRESHOLD_MIN 0.0312
 #define EDGE_THRESHOLD_MAX 0.125
+// Maximum length of edge in each direction
+#define ITERATIONS         12
 
 namespace xen {
 	namespace sren {
@@ -27,12 +36,11 @@ namespace xen {
 		}
 
 		void fxaaStep(Framebuffer& fb, int x, int y, Vec2r inverseScreenSize) {
-			int currentPixel = y*fb.size.x + x;
+			int currentPixelIndex = y*fb.size.x + x;
 
 			// Luma at the current fragment
-			real lumaCenter = rgb2luma(fb.color[currentPixel].rgb);
-			// :TODO: What to do if these are out of bounds?
-			// :TODO: Check if top left or bottom left is fb[0], if bottom left then swap up and down
+			real lumaCenter = rgb2luma(fb.color[currentPixelIndex].rgb);
+
 			// Luma at the four direct neighbours of the current fragment.
 			real lumaDown   = rgb2luma(fb.color[(y+1)*fb.size.x + (x  )].rgb);
 			real lumaUp     = rgb2luma(fb.color[(y-1)*fb.size.x + (x  )].rgb);
@@ -103,16 +111,71 @@ namespace xen {
     		lumaLocalAverage = 0.5*(luma2 + lumaCenter);
 			}
 
-			// :TODO: How to translate this into xen code?
 			// Shift UV in the correct direction by half a pixel.
-			Vec2r currentUv = In.uv;
+			Vec2r currentPixel = Vec2r{x,y};
 			if(isHorizontal){
-    		currentUv.y += stepLength * 0.5;
+    		currentPixel.y += stepLength * 0.5;
 			} else {
-    		currentUv.x += stepLength * 0.5;
+    		currentPixel.x += stepLength * 0.5;
 			}
 
-			// :TODO: Continue from 'First iteration exploration' in tutorial
+			// Compute offset (for each iteration step) in the right direction.
+			Vec2r offset = isHorizontal ? Vec2r{inverseScreenSize.x, 0.0} : Vec2r{0.0,inverseScreenSize.y};
+			// Compute UVs to explore on each side of the edge, orthogonally. The QUALITY allows us to step faster.
+			Vec2r uv1 = currentPixel - offset;
+			Vec2r uv2 = currentPixel + offset;
+
+			// Read the lumas at both current extremities of the exploration segment, and compute the delta wrt to the local average luma.
+			real lumaEnd1 = rgb2luma(fb.color[(uv1.y)*fb.size.x + (uv1.x)].rgb);
+			real lumaEnd2 = rgb2luma(fb.color[(uv2.y)*fb.size.x + (uv2.x)].rgb);
+			lumaEnd1 -= lumaLocalAverage;
+			lumaEnd2 -= lumaLocalAverage;
+
+			// If the luma deltas at the current extremities are larger than the local gradient, we have reached the side of the edge.
+			bool reached1 = abs(lumaEnd1) >= gradientScaled;
+			bool reached2 = abs(lumaEnd2) >= gradientScaled;
+			bool reachedBoth = reached1 && reached2;
+
+			// If the side is not reached, we continue to explore in this direction.
+			if(!reached1){
+			    uv1 -= offset;
+			}
+			if(!reached2){
+			    uv2 += offset;
+			}
+
+			// If both sides have not been reached, continue to explore.
+			if(!reachedBoth){
+
+			  for(int i = 2; i < ITERATIONS; i++){
+			    // If needed, read luma in 1st direction, compute delta.
+			    if(!reached1){
+			      lumaEnd1 = rgb2luma(fb.color[(uv1.y)*fb.size.x + (uv1.x)].rgb);
+			      lumaEnd1 = lumaEnd1 - lumaLocalAverage;
+			    }
+			    // If needed, read luma in opposite direction, compute delta.
+			    if(!reached2){
+			      lumaEnd2 = rgb2luma(fb.color[(uv2.y)*fb.size.x + (uv2.x)].rgb);
+			      lumaEnd2 = lumaEnd2 - lumaLocalAverage;
+			    }
+			    // If the luma deltas at the current extremities is larger than the local gradient, we have eached he side of the edge.
+			    reached1 = abs(lumaEnd1) >= gradientScaled;
+			    reached2 = abs(lumaEnd2) >= gradientScaled;
+			    reachedBoth = reached1 && reached2;
+
+			    // If the side is not reached, we continue to explore in this direction, with a variable quality.
+			    if(!reached1){
+			      uv1 -= offset;// * QUALITY(i);
+			    }
+			    if(!reached2){
+			      uv2 += offset;// * QUALITY(i);
+			    }
+
+			    // If both sides have been reached, stop the exploration.
+			    if(reachedBoth){ break;}
+			  }
+			}
+
 		}
 
 		void PostProcessorAntialias::process(Framebuffer& fb) {
