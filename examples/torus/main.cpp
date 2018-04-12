7e82e0b6262614e5ab50955e5581c7dec34a5466
Jamie Terry <jnterry@ntlworld.com>
2018-04-12 03:06:30 +0100

    Add dynamic lighting to torus demo

--- examples/torus/main.cpp
+++ examples/torus/main.cpp
@@ -4,14 +4,19 @@
 
 xen::Camera3dCylinder                  camera;
 xen::RenderParameters3d                render_params;
-xen::FixedArray<xen::LightSource3d, 2> scene_lights;
+xen::FixedArray<xen::LightSource3d, 3> scene_lights;
 
 xen::FixedArray<xen::VertexAttribute::Type, 3> vertex_spec;
 xen::Mesh                                      mesh_torus;
 xen::Mesh                                      mesh_cube;
 xen::Mesh                                      mesh_axes;
 
-xen::FixedArray<xen::RenderCommand3d, 3> render_commands;
+xen::FixedArray<xen::RenderCommand3d, 6> render_commands;
+
+#define CMD_IDX_TOR_A  0
+#define CMD_IDX_TOR_B  1
+#define CMD_IDX_FLOOR  2
+#define CMD_IDX_LIGHT  3
 
 void initRenderCommands(){
 	xen::clearToZero(render_commands);
@@ -30,6 +35,23 @@ void initRenderCommands(){
 	                                      Mat4r::Identity
 	                                     );
 	render_commands[1].mesh            = mesh_torus;
+
+	render_commands[2].primitive_type  = xen::PrimitiveType::TRIANGLES;
+	render_commands[2].color           = xen::Color::WHITE4f;
+	render_commands[2].model_matrix    = (xen::Translation3d(-0.5_r, -0.5_r, -0.5_r) *
+	                                      xen::Scale3d      (5, 0.1, 5)              *
+	                                      xen::Translation3d(0.0_r, -0.5_r, 0.0_r)
+	                                     );
+	render_commands[2].mesh            = mesh_cube;
+
+	for(u32 i = 0; i < 3; ++i){
+		render_commands[CMD_IDX_LIGHT+i].primitive_type = xen::PrimitiveType::TRIANGLES;
+		render_commands[CMD_IDX_LIGHT+i].color          = xen::Color::BLACK4f;
+		render_commands[CMD_IDX_LIGHT+i].color[i]       = 1.0f;
+		render_commands[CMD_IDX_LIGHT+i].model_matrix   = Mat4r::Identity;
+		render_commands[CMD_IDX_LIGHT+i].mesh           = mesh_cube;
+		render_commands[CMD_IDX_LIGHT+i].flags          = xen::Material::Flags::DisableShadowCast;
+	}
 }
 
 void initCamera(){
@@ -45,7 +67,18 @@ void initCamera(){
 }
 
 void initSceneLights(){
-	render_params.ambient_light = xen::Color3f(0.9f, 0.9f, 0.9f);
+	render_params.ambient_light = xen::Color3f(0.0f, 0.0f, 0.0f);
+
+	for(u32 i = 0; i < 3; ++i){
+		scene_lights[i].type           = xen::LightSource3d::POINT;
+		scene_lights[i].point.position = Vec3r{0.0_r, 0.0_r, 0.0_r};
+		scene_lights[i].point.position[i] = 1.0f;
+		scene_lights[i].color          = xen::Color::BLACK4f;
+		scene_lights[i].color[i]       = 1.0f;
+		scene_lights[i].color.a        = 1.0f;
+		scene_lights[i].attenuation    = {0.0f, 0.0f, 4.0f};
+	}
+	render_params.lights = scene_lights;
 }
 
 void initMeshes(xen::GraphicsDevice* device, xen::ArenaLinear& arena){
@@ -102,11 +135,42 @@ int main(int argc, char** argv){
 			case xen::WindowEvent::Closed:
 				app.device->destroyWindow(app.window);
 				break;
+			case xen::WindowEvent::KeyPressed:
+				switch(event->key.key){
+				case xen::Key::W: // wireframe
+					render_commands[0].primitive_type = xen::PrimitiveType::LINES;
+					render_commands[1].primitive_type = xen::PrimitiveType::LINES;
+					break;
+				case xen::Key::P: // point cloud
+					render_commands[0].primitive_type = xen::PrimitiveType::POINTS;
+					render_commands[1].primitive_type = xen::PrimitiveType::POINTS;
+					break;
+				case xen::Key::F: // filled
+					render_commands[0].primitive_type = xen::PrimitiveType::TRIANGLES;
+					render_commands[1].primitive_type = xen::PrimitiveType::TRIANGLES;
+					break;
+				default: break;
+				}
 			default: break;
 			}
 		}
 		handleCameraInputCylinder(camera, dt, 30);
 		render_params.camera = xen::generateCamera3d(camera);
+
+		for(u32 i = 0; i < 3; ++i){
+			xen::Angle cycle = (100_deg * time * (1.0_r+i*0.1_r)) + 120_deg * i;
+			Vec3r pos = xen::rotated(Vec3r{1,0,0},
+			                         Vec3r::UnitY,
+			                         cycle
+			                        );
+
+			pos.y += xen::sin(cycle + (120_deg*i)) * 0.5_r;
+			scene_lights[i].point.position = pos;
+			render_commands[CMD_IDX_LIGHT+i].model_matrix = (xen::Translation3d(-0.5_r, -0.5_r, -0.5_r) *
+			                                                 xen::Scale3d(0.05_r) *
+			                                                 xen::Translation3d(pos)
+			                                                 );
+		}
 
 		app.device->clear      (app.window, xen::Color{20, 20, 20, 255});
 	  app.device->render     (app.window, viewport, render_params, render_commands);
