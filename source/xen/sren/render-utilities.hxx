17e5d72360fafdafae6d0b6fba9305c09e7484f9
Jamie Terry <jnterry@ntlworld.com>
2018-04-22 22:15:42 +0100

    Add better render debug bounding box function

--- source/xen/sren/render-utilities.hxx
+++ source/xen/sren/render-utilities.hxx
@@ -22,50 +22,55 @@
 #include <cstdio>
 
 namespace xen{
-	// forward declarations
-	struct Camera3d;
-	struct RenderCommand3d;
 
-	namespace sren {
-		struct RenderTargetImpl;
+// forward declarations
+struct Camera3d;
+struct RenderCommand3d;
+namespace sren {
+struct RenderTargetImpl;
 
-		/////////////////////////////////////////////////////////////////////
-		/// \brief Clears the diffuse component of a render target to the
-		/// specified color
-		/////////////////////////////////////////////////////////////////////
-		void clear(RenderTargetImpl& target, Color color);
+/////////////////////////////////////////////////////////////////////
+/// \brief Clears the diffuse component of a render target to the
+/// specified color
+/////////////////////////////////////////////////////////////////////
+void clear(RenderTargetImpl& target, Color color);
 
-		/////////////////////////////////////////////////////////////////////
-		/// \brief Clears the diffuse component of a section of a render target
-		/// to the specified color
-		/////////////////////////////////////////////////////////////////////
-		void clear(RenderTargetImpl& target, const xen::Aabb2u& viewport, Color color);
+/////////////////////////////////////////////////////////////////////
+/// \brief Clears the diffuse component of a section of a render target
+/// to the specified color
+/////////////////////////////////////////////////////////////////////
+void clear(RenderTargetImpl& target, const xen::Aabb2u& viewport, Color color);
 
-		/////////////////////////////////////////////////////////////////////
-		/// \brief Draws debug view of a camera (eg, origin, up dir, look dir, etc),
-		/// from some other camera's perspective
-		/// \param target      The render target to draw to
-		/// \param viewport    The area of the target to draw to
-		/// \param view_camera The camera to use as the perspective to draw from
-		/// \param camera      The camera to draw
-		/// \param scale       The scale of the camera to draw in world units
-		/////////////////////////////////////////////////////////////////////
-		void renderCameraDebug(xen::sren::RenderTargetImpl& target,
-		                       const xen::Aabb2u&           viewport,
-		                       const Camera3d&              view_camera,
-		                       const Camera3d&              camera,
-		                       real                         scale);
+/////////////////////////////////////////////////////////////////////
+/// \brief Draws debug view of a camera (eg, origin, up dir, look dir, etc),
+/// from some other camera's perspective
+/// \param target      The render target to draw to
+/// \param viewport    The area of the target to draw to
+/// \param view_camera The camera to use as the perspective to draw from
+/// \param camera      The camera to draw
+/// \param scale       The scale of the camera to draw in world units
+/////////////////////////////////////////////////////////////////////
+void renderCameraDebug(xen::sren::RenderTargetImpl& target,
+                       const xen::Aabb2u&           viewport,
+                       const Camera3d&              view_camera,
+                       const Camera3d&              camera,
+                       real                         scale);
 
-		/////////////////////////////////////////////////////////////////////
-		/// \brief Renders a bounding box to the screen
-		/////////////////////////////////////////////////////////////////////
-		void renderDebugBoundingBox(RasterizationContext context,
-		                            xen::Aabb3r          aabb,
-		                            xen::Color4f         color = xen::Color::RED4f
-		                           );
+/////////////////////////////////////////////////////////////////////
+/// \brief Renders a bounding box to the screen
+/////////////////////////////////////////////////////////////////////
+void renderDebugBoundingBox(RasterizationContext context,
+                            xen::Aabb3r          aabb,
+                            xen::Color4f         color = xen::Color::RED4f
+                            );
 
-	}
+void renderDebugBoundingBox(xen::sren::RenderTargetImpl& target,
+                            const xen::Aabb2u&           viewport,
+                            const xen::Camera3d&         camera,
+                            xen::Aabb3r                  aabb,
+                            xen::Color4f                 color = xen::Color::RED4f);
 
-}
+} // end of namespace sren
+} // end of namespace xen
 
 #endif
