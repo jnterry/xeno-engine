e17d699aac2713c9defee0d88251d00398175422
Jamie Terry <jnterry@ntlworld.com>
2018-09-22 10:55:10 +0100

    Add a copyFile utility function

--- source/core/src/File.unix.cpp
+++ source/core/src/File.unix.cpp
@@ -11,6 +11,9 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 namespace xen {
 	DateTime getPathModificationTime(const char* path){
@@ -25,6 +28,22 @@ namespace xen {
 	bool pathExists(const char* path){
 		struct stat tmp;
 		return stat(path, &tmp) == 0;
+	}
+
+	bool copyFile(const char* in, const char* out){
+		int fin  = open(in,  O_RDONLY, 0);
+
+		struct stat stat_in;
+		if(fstat(fin, &stat_in) != 0){
+			return false;
+		}
+
+		int fout = open(out, O_WRONLY | O_CREAT | O_TRUNC, stat_in.st_mode & 0777);
+
+		sendfile(fout, fin, 0, stat_in.st_size);
+
+		close(fout);
+		close(fin);
 	}
 }
 
