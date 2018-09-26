78c543f875d80ce0f34a41d6169845f306431a5f
Jamie Terry <jnterry@ntlworld.com>
2018-09-26 23:15:28 +0100

    Add dummy implementation of platform specific log functions

---
+++ source/kernel/src/log.dummy.cpp
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////
+///                      Part of Xeno Engine                             ///
+////////////////////////////////////////////////////////////////////////////
+/// \brief Contains platform specific implementation of kernel logging functions
+///
+/// \ingroup kernel
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef XEN_KERNEL_LOG_DUMMY_CPP
+#define XEN_KERNEL_LOG_DUMMY_CPP
+
+#include "log.hxx"
+#include <xen/core/String.hpp>
+#include <xen/core/memory/utilities.hpp>
+
+bool xke::initLogSubsystem(){
+	return true;
+}
+
+void xke::printLogMsgToStdio(const xen::LogMessage& lm){
+	if(lm.level < xen::LogLevel::DEBUG){
+		return;
+	}
+
+	printf(lm.message);
+}
+
+#endif
