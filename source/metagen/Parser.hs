39ac078f9272ed7ef6f21ffdcb45d0c1af4f68c2
Jamie Terry <jnterry@ntlworld.com>
2019-01-31 00:21:54 +0000

    Fix whitespace related bugs in struct field parser

--- source/metagen/Parser.hs
+++ source/metagen/Parser.hs
@@ -23,7 +23,7 @@ produce val = val <$ string ""
 -- Space consumer, eats all whitespace, fails if there is no whitespace
 -- to be eaten
 sc :: Parser ()
-sc = L.space space1 lineComment blockComment
+sc = L.space (() <$ spaceChar) lineComment blockComment
   where
     lineComment  = L.skipLineComment "//"
     blockComment = L.skipBlockComment "/*" "*/"
@@ -36,8 +36,8 @@ lexeme = L.lexeme sc
 
 -- Tries to parse some lexeme, if possible the token is consumed and true is
 -- returned. Else false is returned
-lexemeMaybe :: Parser a -> Parser Bool
-lexemeMaybe p = True <$ lexeme p <|> produce False
+checkLexeme :: Parser a -> Parser Bool
+checkLexeme p = True <$ lexeme p <|> produce False
 
 integer :: Parser Integer
 integer = lexeme L.decimal
@@ -55,9 +55,12 @@ keywords = [
   "virtual", "volatile", "while"
   ]
 
--- Parses an identifer for a variable or function name, consists of A..Z, a..z, and _
-identifier :: Parser String
-identifier  = (lexeme . try) ( p >>= check)
+keyword :: String -> Parser String
+keyword w = (lexeme . try) (string w <* notFollowedBy (alphaNumChar <|> char '_'))
+
+-- Parses an identifier, but does not consume trailing whitespace
+identifierWord :: Parser String
+identifierWord = try (p >>= check)
   where
     p = (:)
         <$> (letterChar <|> char '_')
@@ -66,26 +69,30 @@ identifier  = (lexeme . try) ( p >>= check)
               then fail $ "Wanted identifier but got keyword " ++ show x
               else return x
 
+-- Parses an identifer lexeme, eg, for a variable, type or function name
+identifier :: Parser String
+identifier  = lexeme identifierWord
 
 -- Parses optionally fully qualified typename, for example, xen::Window
 typename :: Parser Typename
-typename = lexeme ((++) <$> segment <*> (typename <|> produce ""))
+typename = lexeme p
   where
+    p = ((++) <$> segment <*> (p <|> produce ""))
     segment :: Parser String
-    segment  = (++) <$> (string "::" <|> produce "") <*> identifier
+    segment  = (++) <$> (string "::" <|> produce "") <*> identifierWord
 
 
 qualifier :: Parser Qualifier
-qualifier =     Constexpr <$ lexeme (string "constexpr")
-            <|> Const     <$ lexeme (string "const")
-            <|> Static    <$ lexeme (string "static")
-            <|> Volatile  <$ lexeme (string "volatile")
-            <|> Mutable   <$ lexeme (string "mutable")
+qualifier =     Constexpr <$ lexeme (keyword "constexpr")
+            <|> Const     <$ lexeme (keyword "const")
+            <|> Static    <$ lexeme (keyword "static")
+            <|> Volatile  <$ lexeme (keyword "volatile")
+            <|> Mutable   <$ lexeme (keyword "mutable")
 
 indirection :: Parser Indirection
 indirection =
       Reference <$ lexeme (char '&')
-  <|> mkptr     <$> some (lexeme (char '*')) <*> lexemeMaybe (string "const")
+  <|> mkptr     <$> some (lexeme (char '*')) <*> checkLexeme (string "const")
   <|> Direct    <$  produce ""
   where
     mkptr :: String -> Bool -> Indirection
