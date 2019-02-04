132ebcba839571215c5d830598b258191f5a79c8
Jamie Terry <jnterry@ntlworld.com>
2019-02-04 12:19:14 +0000

    Remove old sren postprocessors system
    This will eventually be handled by render to texture then applying
    custom shader

--- source/sren/sources.cmake
+++ source/sren/sources.cmake
@@ -16,9 +16,4 @@ set(XEN_SOURCES_SREN
 	${CMAKE_CURRENT_LIST_DIR}/src/rasterizer3d.cpp
 	${CMAKE_CURRENT_LIST_DIR}/impl/xen/sren/render-debug.hxx
 	${CMAKE_CURRENT_LIST_DIR}/src/render-debug.cpp
-
-	${CMAKE_CURRENT_LIST_DIR}/src/post_processors/InvertColors.cpp
-	${CMAKE_CURRENT_LIST_DIR}/src/post_processors/DisplayDepthBuffer.cpp
-	${CMAKE_CURRENT_LIST_DIR}/src/post_processors/Antialias.cpp
-	${CMAKE_CURRENT_LIST_DIR}/src/post_processors/DepthFog.cpp
 )
