3ac15642450995ee0400d636890810c239155e65
Jamie Terry <jnterry@ntlworld.com>
2019-02-20 15:51:49 +0000

    Enable 2 channel texture support and fix glsl swizzling.
    This now means when 1 or 2 channels are used r = g = b in GLSL, and a
    either defaults to 1 or is read from the texture

--- source/module-gl/src/Texture.cpp
+++ source/module-gl/src/Texture.cpp
@@ -15,39 +15,68 @@
 #include "Texture.hxx"
 #include "ModuleGl.hxx"
 
+void setTextureSwizzlePattern(GLenum texture_type, u32 channels){
+	// OpenGL always internally stores components as R, RG, RGB or RGBA
+	// When channel count is 1 or 2 we want r = g = b in shaders to emulate greyscale
+	// Hence we set a custom swizzle mask to force this behaviour
+	GLint swizzle_mask[4];
+	switch(channels){
+	case 1:
+		swizzle_mask[0] = GL_RED;
+		swizzle_mask[1] = GL_RED;
+		swizzle_mask[2] = GL_RED;
+		swizzle_mask[3] = GL_ONE; // Alpha always 1 if not specified by image
+		break;
+	case 2:
+		swizzle_mask[0] = GL_RED;
+		swizzle_mask[1] = GL_RED;
+		swizzle_mask[2] = GL_RED;
+		swizzle_mask[3] = GL_GREEN; // Alpha specified by image, but as 2nd component
+		break;
+	case 3:
+		swizzle_mask[0] = GL_RED;
+		swizzle_mask[1] = GL_GREEN;
+		swizzle_mask[2] = GL_BLUE;
+		swizzle_mask[3] = GL_ONE; // Alpha always 1 if not specified by image
+		break;
+	case 4:
+		swizzle_mask[0] = GL_RED;
+		swizzle_mask[1] = GL_GREEN;
+		swizzle_mask[2] = GL_BLUE;
+		swizzle_mask[3] = GL_ALPHA;
+		break;
+	}
+	XGL_CHECK(glTexParameteriv(texture_type, GL_TEXTURE_SWIZZLE_RGBA, swizzle_mask));
+}
+
 bool bufferGlTextureData2d(GLenum type, Vec2u size, bool is_floating, u32 channels, const void* data){
 	GLenum data_format;
-	GLenum internal_format;
+	switch(channels){
+	case 1: data_format = GL_RED;  break;
+	case 2: data_format = GL_RG;   break;
+	case 3: data_format = GL_RGB;  break;
+	case 4: data_format = GL_RGBA; break;
+	default:
+		// :TODO: support 2 channel (greyscale and alpha)
+		XenLogError("Invalid texture channel count, got: %i, expected 1,2 or 4");
+		return false;
+	}
 
+	GLenum internal_format;
 	if(is_floating){
+		// :TODO: can we use lower precision float types (16 or 8)
+		// Ideally we want the user to be able to hint to us
+		//
+		// (we could just let them hint "compressed, normal, high dynamic range"
+		// or similar then also apply this to bytes by storing as RGB4 or similar
+		// see table 2 on https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glTexImage2D.xhtml
 		switch(channels){
-		case 1:
-			data_format = GL_RED;
-			internal_format = GL_R32F;
-			break;
-		case 3:
-			data_format = GL_RGB;
-			internal_format = GL_RGB32F;
-			break;
-		case 4:
-			data_format = GL_RGBA;
-			internal_format = GL_RGBA32F;
-			break;
-		default:
-			// :TODO: support 2 channel (greyscale and alpha)
-			XenLogError("Invalid texture channel count, got: %i, expected 1,2 or 4");
-			return false;
+		case 1: internal_format = GL_R32F;    break;
+		case 2: internal_format = GL_RG32F;   break;
+		case 3: internal_format = GL_RGB32F;  break;
+		case 4: internal_format = GL_RGBA32F; break;
 		}
 	} else {
-		switch(channels){
-		case 1: data_format = GL_RED;  break;
-		case 2: data_format = GL_RGB;  break;
-		case 4: data_format = GL_RGBA; break;
-		default:
-			// :TODO: support 2 channel (greyscale and alpha)
-			XenLogError("Invalid texture channel count, got: %i, expected 1,2 or 4");
-			return false;
-		}
 		internal_format = data_format;
 	}
 
@@ -88,6 +117,7 @@ xgl::Texture* doCreateTexture2d(xgl::Texture* result,
 	// Set texture parameters
 	XGL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
 	XGL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+	setTextureSwizzlePattern (GL_TEXTURE_2D, channels);
 
 	return result;
 }
@@ -130,6 +160,7 @@ xgl::Texture* doCreateCubeMap(xgl::Texture* result,
 	XGL_CHECK(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
 	XGL_CHECK(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 	XGL_CHECK(glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE));
+	setTextureSwizzlePattern (GL_TEXTURE_CUBE_MAP, channels);
 
 	return result;
 }
