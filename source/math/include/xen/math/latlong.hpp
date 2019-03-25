61a1c5dd20d9adf8094994bbad78a048e09e5e33
Jamie Terry <jnterry@ntlworld.com>
2019-03-25 12:00:37 +0000

    Add function to clamp latlong values and unit tests

--- source/math/include/xen/math/latlong.hpp
+++ source/math/include/xen/math/latlong.hpp
@@ -6,18 +6,19 @@
 /// \ingroup math
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef XEN_MATH_SPHERICAL_VECTOR_HPP
-#define XEN_MATH_SPHERICAL_VECTOR_HPP
+#ifndef XEN_MATH_LATLONG_HPP
+#define XEN_MATH_LATLONG_HPP
 
 #include <xen/core/intrinsics.hpp>
 #include <xen/math/quaternion.hpp>
 #include <xen/math/vector.hpp>
+#include <xen/math/angle.hpp>
 
 namespace xen {
 
 	typedef Vec2<xen::Angle> LatLong;
 
-	Vec3r toCartesian(const LatLong& v, real radius = 1){
+	inline Vec3r toCartesian(const LatLong& v, real radius = 1){
 		real cx = xen::cos(v.x);
 		return radius * Vec3r{
 			xen::cos(v.y) * cx,
@@ -25,7 +26,7 @@ namespace xen {
 			xen::sin(v.y) * -cx,
 		};
 	}
-  LatLong toLatLong(Vec3r v){
+  inline LatLong toLatLong(Vec3r v){
 	  v = xen::normalized(v);
 		LatLong result;
 		result.x = xen::asin(v.y);
@@ -33,14 +34,36 @@ namespace xen {
 		result.y = xen::atan2(-v.z / cx, v.x / cx);
 		return result;
 	}
+
+	/// \brief Clamps a lat long into a range such that there exists a unique
+	/// representation for any point on the surface of the sphere
+	/// lat  (x) becomes between -90_deg and 90_deg,
+	/// long (y) becomes between -180_deg and 179.99999_deg
+	inline LatLong clamp(LatLong ll){
+		// Clamp lat to between -180 and 180
+		xen::Angle lat = xen::clamp(ll.x + 180_deg) - 180_deg;
+		xen::Angle lon = ll.y;
+
+		// If |lat| is greater than 90, then spin 180_deg around the equator,
+		// and change, eg, 95deg -> 85deg to reflect min distance from equator
+		if(lat < -90_deg){
+			lat  = -180_deg - lat;
+			lon += 180_deg;
+		} else if (lat > 90_deg){
+			lat  = 180_deg - lat;
+			lon += 180_deg;
+		}
+
+		return { lat, xen::clamp(lon + 180_deg) - 180_deg };
+	}
 }
 
 template<typename T>
-xen::LatLong operator+(xen::LatLong& a, xen::LatLong& b){
+inline xen::LatLong operator+(xen::LatLong& a, xen::LatLong& b){
 	return { a.x + b.x, a.y + b.y };
 }
 template<typename T>
-xen::LatLong operator-(const xen::LatLong& a, const xen::LatLong& b){
+inline xen::LatLong operator-(const xen::LatLong& a, const xen::LatLong& b){
 	return { a.x - b.x, a.y - b.y };
 }
 
--- tests/math/latlong.cpp
+++ tests/math/latlong.cpp
@@ -40,6 +40,40 @@ TEST_CASE("latlong, LatLong to Cartesian", "[math][latlong]"){
 	CHECK(xen::toCartesian(xen::LatLong{   45_deg,    0_deg}) == xen::normalized(Vec3r{ 1, 1, 0}));
 }
 
+TEST_CASE("clamp LatLong", "[math][latlong]"){
+	// Test transition points
+	// North pole
+	CHECK(xen::clamp(xen::LatLong{  85_deg,  45_deg}) == xen::LatLong{  85_deg,   45_deg });
+	CHECK(xen::clamp(xen::LatLong{  90_deg,  45_deg}) == xen::LatLong{  90_deg,   45_deg });
+	CHECK(xen::clamp(xen::LatLong{  95_deg,  45_deg}) == xen::LatLong{  85_deg, -135_deg });
+
+	// South pole
+	CHECK(xen::clamp(xen::LatLong{ -85_deg,  45_deg}) == xen::LatLong{ -85_deg,   45_deg });
+	CHECK(xen::clamp(xen::LatLong{ -90_deg,  45_deg}) == xen::LatLong{ -90_deg,   45_deg });
+	CHECK(xen::clamp(xen::LatLong{ -95_deg,  45_deg}) == xen::LatLong{ -85_deg, -135_deg });
+
+	// -180/180 singularity
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  179_deg}) == xen::LatLong{ 0_deg,   179_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  180_deg}) == xen::LatLong{ 0_deg,  -180_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  181_deg}) == xen::LatLong{ 0_deg,  -179_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg, -179_deg}) == xen::LatLong{ 0_deg,  -179_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg, -180_deg}) == xen::LatLong{ 0_deg,  -180_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg, -181_deg}) == xen::LatLong{ 0_deg,   179_deg });
+
+	// Large longitude values
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  270_deg}) == xen::LatLong{ 0_deg, - 90_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  360_deg}) == xen::LatLong{ 0_deg,    0_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  450_deg}) == xen::LatLong{ 0_deg,   90_deg });
+	CHECK(xen::clamp(xen::LatLong{ 0_deg,  540_deg}) == xen::LatLong{ 0_deg, -180_deg });
+
+	// Large latitude
+	CHECK(xen::clamp(xen::LatLong{ 180_deg, 0_deg}) == xen::LatLong{   0_deg, -180_deg });
+	CHECK(xen::clamp(xen::LatLong{ 200_deg, 0_deg}) == xen::LatLong{ -20_deg, -180_deg });
+	CHECK(xen::clamp(xen::LatLong{ 270_deg, 0_deg}) == xen::LatLong{ -90_deg,    0_deg });
+	CHECK(xen::clamp(xen::LatLong{ 300_deg, 0_deg}) == xen::LatLong{ -60_deg,    0_deg });
+	CHECK(xen::clamp(xen::LatLong{ 360_deg, 0_deg}) == xen::LatLong{   0_deg,    0_deg });
+}
+
 TEST_CASE("latlong, Cartesian -> LatLong -> Cartesian", "[math][latlong]"){
 	Vec3r cartesian;
 	for(cartesian.x = -10.0_r; cartesian.x <= 10.0_r; cartesian.x += 1.0_r){
